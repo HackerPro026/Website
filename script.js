e1a05856fa52db76cbd2abbd42125cbf7da092f6
HackerPro026 <coyoushaei@gmail.com>
2018-03-05 12:56:57 -0800

    changed it back to original

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 
 function alert2() {
     var encode = document.querySelector("#first-name").value;
-    encode = encode.split(''); encode = encode.reverse(); encode = encode.join(''); alert(encode);
+    alert(encode);
 }
